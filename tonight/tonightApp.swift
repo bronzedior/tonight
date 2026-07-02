5681d2e0e0efc3886bc7e1153a4d94447b749198
Yuki Resky Damanik <yukidamanik0419@gmail.com>
2026-07-02 11:51:37 +0700

    Initial Commit

---
+++ tonight/tonightApp.swift
@@ -0,0 +1,17 @@
+//
+//  tonightApp.swift
+//  tonight
+//
+//  Created by Yuki Damanik on 02/07/26.
+//
+
+import SwiftUI
+
+@main
+struct tonightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
